a18edac0f8e0f6383a2f8a9c89edd1c61f35a5a1
warre <warre.provoost@ugent.be>
2024-03-25 17:35:48 +0100

    citymap

--- docs/components/cityMap.js
+++ docs/components/cityMap.js
@@ -1,28 +1,35 @@
 import { select } from 'npm:d3-selection';
 import { geoPath, geoMercator } from 'npm:d3-geo';
+import {json} from 'npm:d3';
 
 export function getCitySVG(){
-    const cityData = require('../data/sectoren-circulatieplan-gent.geojson');
-
+    const width = 900;
+    const height = 500;
 
     const svg = select('body')
         .append('svg')
-        .attr('width', 800)
-        .attr('height', 600);
+        .attr('width', width)
+        .attr('height', height);
 
     const projection = geoMercator()
-        .fitSize([800, 600], cityData);
 
     const path = geoPath().projection(projection);
+    json("../data/geo.json", function(err, geojson) {
+
+        projection.fitSize([width,height],geojson);
+
+        svg.append("path").attr("d", path(geojson));
+
+    })
 
 // Draw map
-    svg.selectAll('path')
+    /*svg.selectAll('path')
         .data(cityData.features)
         .enter()
         .append('path')
         .attr('d', path)
         .style('stroke', 'black')
-        .style('fill', 'lightblue');
+        .style('fill', 'lightblue');*/
     return svg
 }
 
